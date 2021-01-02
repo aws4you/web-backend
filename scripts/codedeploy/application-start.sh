f7b8d6c951a76c9bf8cf8e632b4160ba65df3cca
Andrew G. Saushkin <asaushkin@gmail.com>
2021-01-02 12:16:23 +0300

    Remove ampersand in the app run

--- scripts/codedeploy/application-start.sh
+++ scripts/codedeploy/application-start.sh
@@ -6,4 +6,4 @@ set -ex
 
 cd ${APP_HOME} || exit
 
-DJANGO_ENV=DEV GUNICORN_CMD_ARGS="--bind=127.0.0.1:8000 --workers=1" gunicorn -D core.wsgi &
+DJANGO_ENV=DEV GUNICORN_CMD_ARGS="--bind=127.0.0.1:8000 --workers=1" gunicorn -D core.wsgi
