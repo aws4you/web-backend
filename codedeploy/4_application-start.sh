6714b0ad08ea2db87342d3416bfd6df23e5e735b
Andrew G. Saushkin <asaushkin@gmail.com>
2021-01-02 18:15:41 +0300

    Force redirect from http to https

--- codedeploy/4_application-start.sh
+++ codedeploy/4_application-start.sh
@@ -10,7 +10,7 @@ cd ${APP_HOME} || exit
 
 gunicorn -c codedeploy/conf/gunicorn-${DJANGO_ENV}.conf.py -D core.wsgi
 
-envsubst < codedeploy/conf/nginx.conf > $NGINX_AVAILABLE_APP_CONF
+envsubst '$APP_DOMAIN $DJANGO_ENV $APP_HOME' < codedeploy/conf/nginx.conf > $NGINX_AVAILABLE_APP_CONF
 ln -sf $NGINX_AVAILABLE_APP_CONF $NGINX_ENABLED_APP_CONF
 
 service nginx reload
