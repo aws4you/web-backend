e1dd01f5a8e43b2205f12ef5d617b3a9ea060d1c
Andrew G. Saushkin <asaushkin@gmail.com>
2021-01-02 20:48:25 +0300

    Add topic unit test

--- contactmanager/tests.py
+++ contactmanager/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from collections import OrderedDict
 
-# Create your tests here.
+from django.test import TestCase, RequestFactory
+from rest_framework.test import APITestCase
+from contactmanager.views import TopicsList
+from pprint import pprint
+
+class ContactManagerTests(APITestCase):
+    fixtures = ['topic.json', 'webpage.json', 'access-record.json']
+
+    def setUp(self) -> None:
+        self.factory = RequestFactory()
+
+    def topics_list(self) -> OrderedDict:
+        request = self.factory.get('/contactmanager/topic/list/')
+
+        response = TopicsList.as_view()(request)
+
+        return response.data
+
+    def test_topic_list_count(self):
+        self.assertTrue(len(self.topics_list()) == 5)
