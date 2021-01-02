0a3f7df2599b099f8fe8c31f9c8ddf5cbb22ecbf
Andrew G. Saushkin <asaushkin@gmail.com>
2021-01-02 15:59:00 +0300

    Move to unix socket

--- codedeploy/conf/gunicorn-DEV.conf.py
+++ codedeploy/conf/gunicorn-DEV.conf.py
@@ -1,4 +1,6 @@
 import os
 
-bind = f"127.0.0.1:{os.getenv('APP_PORT', 8001)}"
+#bind = f"127.0.0.1:{os.getenv('APP_PORT', 8001)}"
+
+bind = f"unix:/tmp/web-backend-{os.getenv('DJANGO_ENV', 'NONE')}.sock"
 workers = 1
--- codedeploy/conf/gunicorn-PROD.conf.py
+++ codedeploy/conf/gunicorn-PROD.conf.py
@@ -1,7 +1,9 @@
 import multiprocessing
 import os
 
-bind = f"127.0.0.1:{os.getenv('APP_PORT', 8000)}"
+#bind = f"127.0.0.1:{os.getenv('APP_PORT', 8000)}"
+bind = f"unix:/tmp/web-backend-{os.getenv('DJANGO_ENV', 'NONE')}.sock"
+
 #workers = multiprocessing.cpu_count() * 2 + 1
 
 # since we are not in "real" we use only one CPU
