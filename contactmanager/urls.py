3ef78125af685e8fd7ef6dcc2c14ad6ad3660f6a
Andrew G. Saushkin <asaushkin@gmail.com>
2021-01-02 20:53:31 +0300

    Delete templates/web UI

--- contactmanager/urls.py
+++ contactmanager/urls.py
@@ -1,11 +1,7 @@
-from django.conf.urls import url, include
-from contactmanager.views import index, help, TopicsList
+from django.conf.urls import url
+from contactmanager.views import TopicsList
 
 urlpatterns = [
-    url(r'^$', index),
-    url(r'^help/', help),
-
     url(r'topic/list/$', TopicsList.as_view()),
     #url(r'topic/detail/(?P<pk>[0-9]+)/$', TopicDetail.as_view()),
-
 ]
--- contactmanager/views.py
+++ contactmanager/views.py
@@ -1,28 +1,9 @@
-from django.shortcuts import render
 from rest_framework import generics
 
-# Create your views here.
 from contactmanager.models import Topic
 from contactmanager.serializers import TopicSerializer
 
 
-def index(request):
-
-    vars = {
-        "insert_me": "hello!"
-    }
-
-    return render(request, "index.html", context=vars)
-
-def help(request):
-    return render(request, "help.html")
-
 class TopicsList(generics.ListCreateAPIView):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
-
-    # def get_object(self):
-    #     pk = self.kwargs['pk']
-    #     pj = Topic.objects.filter(id=pk)[0]
-    #
-    #     return self.serializer_class.Meta.model.objects.get(id=pk)
