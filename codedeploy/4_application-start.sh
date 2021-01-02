4a69f9a487527cdb6dae3c96c3d4370195deea12
Andrew G. Saushkin <asaushkin@gmail.com>
2021-01-02 15:31:27 +0300

    Add nginx config

--- codedeploy/4_application-start.sh
+++ codedeploy/4_application-start.sh
@@ -7,3 +7,8 @@ set -ex
 cd ${APP_HOME} || exit
 
 gunicorn -c codedeploy/conf/gunicorn-${DJANGO_ENV}.conf.py -D core.wsgi
+
+envsubst < codedeploy/conf/nginx.conf > $NGINX_AVAILABLE_APP_CONF
+ln -sf $NGINX_AVAILABLE_APP_CONF $NGINX_ENABLED_APP_CONF
+
+service nginx reload
--- codedeploy/functions.sh
+++ codedeploy/functions.sh
@@ -1,7 +1,6 @@
 export DJANGO_ENV=${DEPLOYMENT_GROUP_NAME:-LOCAL}
 export APP_HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pwd)
 
-
 case "$DJANGO_ENV" in
 PROD)
   export APP_PORT=8000
@@ -16,3 +15,7 @@ DEV)
   exit 1
   ;;
 esac
+
+export NGINX_CONF=backend-${DJANGO_ENV}.conf
+export NGINX_AVAILABLE_APP_CONF=/etc/nginx/sites-available/${NGINX_CONF}
+export NGINX_ENABLED_APP_CONF=/etc/nginx/sites-enabled/${NGINX_CONF}
