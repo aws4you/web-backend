4708627c9a71784fe4f16258277a4d7e732258cc
Andrew G. Saushkin <asaushkin@gmail.com>
2021-01-04 17:26:54 +0300

    Add DJANGO_ENV to .envrc deployment path

--- codedeploy/5_validate-service.sh
+++ codedeploy/5_validate-service.sh
@@ -9,4 +9,5 @@ cd ${APP_HOME} || exit
 curl -f --unix-socket $GUNICORN_SOCKET_PATH http://127.0.0.1/admin/
 
 
-echo "DJANGO_LATEST_${DJANGO_ENV}=$(pwd)" > /etc/default/env.${DJANGO_ENV}.deployment
+echo "export DJANGO_LATEST_${DJANGO_ENV}=$(pwd)" > /etc/default/env.${DJANGO_ENV}.deployment
+echo "export DJANGO_ENV=${DJANGO_ENV}" > .envrc
