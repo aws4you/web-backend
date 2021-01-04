488935f4fbed6620c092d2e1d083e47b4d7a8cec
Andrew G. Saushkin <asaushkin@gmail.com>
2021-01-04 17:14:16 +0300

    Add the latest successful deployment path

--- codedeploy/5_validate-service.sh
+++ codedeploy/5_validate-service.sh
@@ -7,3 +7,6 @@ set -ex
 cd ${APP_HOME} || exit
 
 curl -f --unix-socket $GUNICORN_SOCKET_PATH http://127.0.0.1/admin/
+
+
+echo "DJANGO_LATEST_${DJANGO_ENV}=$(pwd)" > /etc/default/env.${DJANGO_ENV}.deployment
